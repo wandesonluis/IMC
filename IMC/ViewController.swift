b918c5f8969208a7ec728ff5ea466e50d54f419a
wandesonluis <wandeson_luis@hotmail.com>
2018-06-16 16:22:12 -0300

    Projeto IMC

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
+    
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!){
+           imc = weight / (height*height)
+            showResults()
+        }
+    }
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "magreza"
+            case 16..<18.5:
+                result = "Abaixo do Peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        lbResult.text = "\(Int(imc)): \(result)"
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+        view.endEditing(true)
+    }
 
 }
 
